9df7ca8af599c3b1000010006294b6bc05fb81a2
Alden <aj.molina092@gmail.com>
2019-02-20 10:45:48 -0500

    First commit

---
+++ MVCVirtualPets/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCVirtualPets.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello"; 
+        }
+        
+    }
+}
---
+++ MVCVirtualPets/Controllers/PetController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCVirtualPets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCVirtualPets.Controllers
+{
+    public class PetController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    
+        public ViewResult Details(int id)
+        {
+          
+            var pets = new Dictionary<int, Pet>()
+            {
+                { 1, new Pet() { Name = "Doofus" } },
+                { 2, new Pet() { Name = "Graile" } }
+            };
+            
+            var model = pets[id];
+            return View(model);
+
+        }
+    }
+}
